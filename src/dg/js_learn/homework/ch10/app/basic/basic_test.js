cf35108e9efb2750a50012121019a84cfd6ee7a0
XenRon <xenron@outlook.com>
2016-09-25 23:24:51 +0000

    msg

--- src/dg/js_learn/homework/ch10/app/basic/basic_test.js
+++ src/dg/js_learn/homework/ch10/app/basic/basic_test.js
@@ -6,6 +6,7 @@ describe('myAppBasic module', function() {
   var $_scope;
   var $_compile;
   var template;
+  
   beforeEach(function() {
     module('myAppBasic');
   });
@@ -19,31 +20,10 @@ describe('myAppBasic module', function() {
   });
 
   it('should equal 4', function() {
-    element = angular.element("<div eh-simple1>{{2+2}}</div>");
+    element = angular.element("<div>{{2+2}}</div>");
     $_compile(element)($_scope);
     $_scope.$digest();
     expect(element.html()).toBe("4");
   });
-  
-  // describe('view1 controller', function(){
-  //   it('should ....', inject(function($controller) {
-  //     expect(true).toBe(true);
-  //   }));
-
-  //   it("should equal 4 -inner", function() {
-  //     $scope.$digest();
-  //     expect(element1.html()).toBe("4");
-  //   });
-  //   it("should add a class of plain", function() {
-  //     $scope.$digest();
-  //     expect(element2.hasClass("plain")).toBe(true);
-  //   });
-  //   it("should respond to a click", function() {
-  //     element3.triggerHandler('click');
-  //     // expect(element3.scope().clicked).toBe(true);
-  //     expect(true).toBe(true);
-  //   });
-
-  // });
 
 });
