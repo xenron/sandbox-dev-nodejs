705e799cf014f0288b3594a2415c3060ad0bd0a6
XenRon <xenron@outlook.com>
2016-09-26 00:19:04 +0000

    msg

--- src/dg/js_learn/homework/ch10/app/controller/controller_test.js
+++ src/dg/js_learn/homework/ch10/app/controller/controller_test.js
@@ -4,67 +4,69 @@ describe('myAppController module', function() {
 
   beforeEach(module('myAppController'));
   
-  // var $controller;
+  var $controller;
 
-  // beforeEach(inject(function(_$controller_){
-  //   // The injector unwraps the underscores (_) from around the parameter names when matching
-  //   $controller = _$controller_;
-  // }));
+  beforeEach(inject(function(_$controller_){
+    // The injector unwraps the underscores (_) from around the parameter names when matching
+    $controller = _$controller_;
+  }));
 
   
-  it('should be defined ...', inject(function($controller) {
+  it('should be defined ...', inject(function(_$controller_) {
     //spec body
-    var view1Ctrl = $controller('myAppControllerCtrl01');
+    var view1Ctrl = _$controller_('myAppControllerCtrl01');
     expect(view1Ctrl).toBeDefined();
   }));
   
   describe('myAppController controller', function(){
-    it('should be defined ....', inject(function($controller) {
+    it('should be defined ....', inject(function(_$controller_) {
       //spec body
-      var view1Ctrl = $controller('myAppControllerCtrl01');
+      var view1Ctrl = _$controller_('myAppControllerCtrl01');
       expect(view1Ctrl).toBeDefined();
     }));
   });
   
-  // describe('$scope.grade', function() {
-  //   it('sets the strength to "strong" if the password length is >8 chars', function() {
-  //     var $scope = {};
-  //     var controller = $controller('PasswordController', { $scope: $scope });
-  //     $scope.password = 'longerthaneightchars';
-  //     $scope.grade();
-  //     expect($scope.strength).toEqual('strong');
-  //   });
+  describe('$scope.grade', function() {
     
-  //   it('sets the strength to "weak" if the password length <3 chars', function() {
-  //     var $scope = {};
-  //     var controller = $controller('PasswordController', { $scope: $scope });
-  //     $scope.password = 'a';
-  //     $scope.grade();
-  //     expect($scope.strength).toEqual('weak');
-  //   });
+    it('sets the strength to "strong" if the password length is >8 chars', function() {
+      var $scope = {};
+      var controller = $controller('PasswordController', { $scope: $scope });
+      $scope.password = 'longerthaneightchars';
+      $scope.grade();
+      expect($scope.strength).toEqual('strong');
+    });
     
-  // });
+    it('sets the strength to "weak" if the password length <3 chars', function() {
+      var $scope = {};
+      var controller = $controller('PasswordController', { $scope: $scope });
+      $scope.password = 'a';
+      $scope.grade();
+      expect($scope.strength).toEqual('weak');
+    });
+    
+  });
   
-  // describe('$scope.grade', function() {
-  //   var $scope, controller;
+  describe('$scope.grade', function() {
+  
+    var $scope, controller;
 
-  //   beforeEach(function() {
-  //     $scope = {};
-  //     controller = $controller('PasswordController', { $scope: $scope });
-  //   });
+    beforeEach(function() {
+      $scope = {};
+      controller = $controller('PasswordController', { $scope: $scope });
+    });
 
-  //   it('sets the strength to "strong" if the password length is >8 chars', function() {
-  //     $scope.password = 'longerthaneightchars';
-  //     $scope.grade();
-  //     expect($scope.strength).toEqual('strong');
-  //   });
+    it('sets the strength to "strong" if the password length is >8 chars', function() {
+      $scope.password = 'longerthaneightchars';
+      $scope.grade();
+      expect($scope.strength).toEqual('strong');
+    });
 
-  //   it('sets the strength to "weak" if the password length <3 chars', function() {
-  //     $scope.password = 'a';
-  //     $scope.grade();
-  //     expect($scope.strength).toEqual('weak');
-  //   });
-  // });
+    it('sets the strength to "weak" if the password length <3 chars', function() {
+      $scope.password = 'a';
+      $scope.grade();
+      expect($scope.strength).toEqual('weak');
+    });
+  });
 
 
 });
