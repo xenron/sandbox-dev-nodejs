3a30c8136f04e2e8f5d56ebcb2601d5fb91176f8
XenRon <xenron@outlook.com>
2016-09-26 00:00:58 +0000

    msg

--- src/dg/js_learn/homework/ch10/app/controller/controller.js
+++ src/dg/js_learn/homework/ch10/app/controller/controller.js
@@ -6,16 +6,16 @@ app.controller('myAppControllerCtrl01', [function() {
 
 }]);
 
-app.controller('PasswordController', function PasswordController($scope) {
-  $scope.password = '';
-  $scope.grade = function() {
-    var size = $scope.password.length;
-    if (size > 8) {
-      $scope.strength = 'strong';
-    } else if (size > 3) {
-      $scope.strength = 'medium';
-    } else {
-      $scope.strength = 'weak';
-    }
-  };
-});
+// app.controller('PasswordController', function PasswordController($scope) {
+//   $scope.password = '';
+//   $scope.grade = function() {
+//     var size = $scope.password.length;
+//     if (size > 8) {
+//       $scope.strength = 'strong';
+//     } else if (size > 3) {
+//       $scope.strength = 'medium';
+//     } else {
+//       $scope.strength = 'weak';
+//     }
+//   };
+// });
