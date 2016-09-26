12341cb0b005efb442dd2de9dacc2850ccdbac8b
XenRon <xenron@outlook.com>
2016-09-26 05:33:46 +0000

    msg

--- src/dg/js_learn/homework/ch10/app/template1/tabsSpec.js
+++ src/dg/js_learn/homework/ch10/app/template1/tabsSpec.js
@@ -36,54 +36,54 @@ describe('tabs', function() {
   }));
 
 
-  // it('should bind the content', function() {
-  //   var contents = elm.find('div.tab-content div.tab-pane');
+  it('should bind the content', function() {
+    var contents = elm.find('div.tab-content div.tab-pane');
 
-  //   expect(contents.length).toBe(2);
-  //   expect(contents.eq(0).text()).toBe('first content is ');
-  //   expect(contents.eq(1).text()).toBe('second content is ');
+    expect(contents.length).toBe(2);
+    expect(contents.eq(0).text()).toBe('first content is ');
+    expect(contents.eq(1).text()).toBe('second content is ');
 
-  //   scope.$apply(function() {
-  //     scope.first = 123;
-  //     scope.second = 456;
-  //   });
+    scope.$apply(function() {
+      scope.first = 123;
+      scope.second = 456;
+    });
 
-  //   expect(contents.eq(0).text()).toBe('first content is 123');
-  //   expect(contents.eq(1).text()).toBe('second content is 456');
-  // });
-
-
-  // it('should set active class on title', function() {
-  //   var titles = elm.find('ul.nav-tabs li');
-
-  //   expect(titles.eq(0)).toHaveClass('active');
-  //   expect(titles.eq(1)).not.toHaveClass('active');
-  // });
+    expect(contents.eq(0).text()).toBe('first content is 123');
+    expect(contents.eq(1).text()).toBe('second content is 456');
+  });
 
 
-  // it('should set active class on content', function() {
-  //   var contents = elm.find('div.tab-content div.tab-pane');
+  it('should set active class on title', function() {
+    var titles = elm.find('ul.nav-tabs li');
 
-  //   expect(contents.eq(0)).toHaveClass('active');
-  //   expect(contents.eq(1)).not.toHaveClass('active');
-  // });
+    expect(titles.eq(0)).toHaveClass('active');
+    expect(titles.eq(1)).not.toHaveClass('active');
+  });
 
 
-  // it('should change active pane when title clicked', function() {
-  //   var titles = elm.find('ul.nav-tabs li');
-  //   var contents = elm.find('div.tab-content div.tab-pane');
+  it('should set active class on content', function() {
+    var contents = elm.find('div.tab-content div.tab-pane');
 
-  //   // click the second tab
-  //   titles.eq(1).find('a').click();
+    expect(contents.eq(0)).toHaveClass('active');
+    expect(contents.eq(1)).not.toHaveClass('active');
+  });
 
-  //   // second title should be active
-  //   expect(titles.eq(0)).not.toHaveClass('active');
-  //   expect(titles.eq(1)).toHaveClass('active');
 
-  //   // second content should be active
-  //   expect(contents.eq(0)).not.toHaveClass('active');
-  //   expect(contents.eq(1)).toHaveClass('active');
-  // });
+  it('should change active pane when title clicked', function() {
+    var titles = elm.find('ul.nav-tabs li');
+    var contents = elm.find('div.tab-content div.tab-pane');
+
+    // click the second tab
+    titles.eq(1).find('a').click();
+
+    // second title should be active
+    expect(titles.eq(0)).not.toHaveClass('active');
+    expect(titles.eq(1)).toHaveClass('active');
+
+    // second content should be active
+    expect(contents.eq(0)).not.toHaveClass('active');
+    expect(contents.eq(1)).toHaveClass('active');
+  });
 });
 
 
