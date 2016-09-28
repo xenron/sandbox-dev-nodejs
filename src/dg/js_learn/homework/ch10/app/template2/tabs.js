e109bcf0139a62ed445e6cb70216cbad4723853b
XenRon <xenron@outlook.com>
2016-09-28 06:40:25 +0000

    msg

--- src/dg/js_learn/homework/ch10/app/template2/tabs.js
+++ src/dg/js_learn/homework/ch10/app/template2/tabs.js
@@ -1,44 +1,11 @@
+var tabs = angular.module('notepad', []);
 
-var app = angular.module('notepad',[]);
-
-app.directive('newelement',function(){
-	return {
-		restrict:'AE',
-		scope:{},
-		templateUrl:'template2/tabs.html',
-		link:function(scope,elem,attrs){
-			scope.editMode = false;
-			scope.restore = function(){
-				scope.editMode = false;
-				scope.index = -1;
-				scope.noteText = '';
-			};
-		   scope.openEditor = function(index){
-				scope.editMode = true;
-			   if(index!==undefined){
-				   scope.noteText= scope.notes[index].content;
-				   scope.index = index;
-			   }else {
-				   scope.noteText = undefined;
-			   }
-		   };
-		   scope.save = function(){
-			   if(scope.noteText!==''&&scope.noteText!==undefined){
-				   var note ={};
-				   note.title = scope.noteText.length>10?scope.noteText.substring(0,10)+'...':scope.noteText;
-				   note.content = scope.noteText;
-				   note.id = scope.index!=-1?scope.index:scope.notes.length;
-				   scope.notes[note.id]=note;
-			   }
-			   scope.restore();
-		   }
-		   var editor = angular.element(elem.children().eq(1));
-		   scope.restore();
-		   scope.notes = [];
-		   editor.bind('keyup keydown',function(){
-			   scope.noteText = editor.text().trim();
-		   });
-
-		}
-	}
+tabs.directive('tabs', function() {
+  return {
+    restrict: 'E',
+    transclude: true,
+    scope: {},
+    templateUrl: 'template2/tabs.html',
+    replace: true
+  };
 });
--- src/dg/js_learn/homework/ch10/app/template2/tabsSpec.js
+++ src/dg/js_learn/homework/ch10/app/template2/tabsSpec.js
@@ -1,50 +1,24 @@
-describe('notepad', function() {
-  var elm, scope, compile;
+describe('tabs', function() {
+  var elm, scope;
 
   // load the tabs code
   beforeEach(module('notepad'));
-  // beforeEach(module('notepad'));
 
   // load the templates
   beforeEach(module('template2/tabs.html'));
-  // beforeEach(module('notepad/template.html'));
 
   beforeEach(inject(function($rootScope, $compile) {
     // we might move this tpl into an html file as well...
-    elm = angular.element('<newelement></newelement>');
-      // '<notepad />');
+    elm = angular.element('<div></div>');
 
     scope = $rootScope;
-    compile = $compile;
     $compile(elm)(scope);
     scope.$digest();
   }));
 
 
-  it('should create notepad', function() {
-    
-
-    // console.log(elm.find('ul li a').length);
-    // console.log(elm.find('a').length);
-    // console.log(elm.find('a'));
-
-    // scope.$apply(function() {
-    //   scope.notes = [];
-    //   var note ={};
-    //   note.title = "test_title";
-    //   note.content = "test_content";
-    //   note.id = 1;
-    //   scope.notes[note.id]=note;
-    // });
-
-    // elm = angular.element('<newelement></newelement>');
-    // compile(elm)(scope);
-    // scope.$digest();
-
-    // console.log(elm.find('ul li a').length);
-    // console.log(elm.find('a').length);
-    // console.log(elm.find('a'));
+  it('should create clickable titles', inject(function($compile, $rootScope) {
     expect(true).toBe(true);
-
-  });
+  }));
 });
+
