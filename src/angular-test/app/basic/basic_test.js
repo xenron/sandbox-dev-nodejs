4df2e6dbec698424acc0216d4797147a88c251e8
xenron <xenron@hotmail.com>
2016-10-01 13:59:43 +0900

    msg

--- src/angular-test/app/basic/basic_test.js
+++ src/angular-test/app/basic/basic_test.js
@@ -1,29 +1,70 @@
 'use strict';
 
-describe('appBasic module', function() {
-
-  var element;
-  var $_scope;
-  var $_compile;
+describe('appBasic module1', function() {
   var template;
-  
+  var $controller;
+  var $rootScope;
+  var $scope;
+  var $compile;
+
   beforeEach(function() {
     module('appBasic');
+    inject(function(_$compile_, _$rootScope_, _$templateCache_) {
+      $scope = _$rootScope_;
+      $compile = _$compile_;
+    });
   });
-  beforeEach(inject(function($compile, $rootScope, $templateCache) {
-    $_scope = $rootScope;
-    $_compile = $compile;
-  }));
 
   it('Should be true ...', function() {
     expect(true).toBe(true);
     expect(true).toBeTruthy();
   });
+});
+
+describe('appBasic module2', function() {
+  var template;
+  var $controller;
+  var $rootScope;
+  var $scope;
+  var $compile;
+
+  beforeEach(function() {
+    module('appBasic');
+    inject(function (_$injector_) {
+      $rootScope = _$injector_.get('$rootScope');
+      $scope = $rootScope.$new();
+      // $controller = _$injector_.get('$controller')('appBassicController', {$scope: $scope});
+    });
+  });
+
+  it('Should be true ...', function() {
+    expect(true).toBe(true);
+    expect(true).toBeTruthy();
+  });
+});
+
+describe('appBasic module3', function() {
+
+  var element;
+  var $scope;
+  var $compile;
+  var template;
+  var $controller;
+  var $rootScope;
+  var $scope;
+  
+  beforeEach(function() {
+    module('appBasic');
+  });
+  beforeEach(inject(function(_$compile_, _$rootScope_, _$templateCache_) {
+    $scope = _$rootScope_;
+    $compile = _$compile_;
+  }));
 
   it('should equal 4', function() {
     element = angular.element("<div>{{2+2}}</div>");
-    $_compile(element)($_scope);
-    $_scope.$digest();
+    $compile(element)($scope);
+    $scope.$digest();
     expect(element.html()).toBe("4");
   });
   
