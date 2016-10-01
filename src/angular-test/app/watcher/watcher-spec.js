8266b1e72454def3d117bb258a6cac1d63a988a4
xenron <xenron@hotmail.com>
2016-10-01 22:34:39 +0900

    msg

--- src/angular-test/app/watcher/watcher-spec.js
+++ src/angular-test/app/watcher/watcher-spec.js
@@ -72,39 +72,4 @@ describe('appWatcher module', function () {
         });
     });
 
-    // describe('Watchers', function () {
-    //     it('Should instantiate slices to 8', function () {
-    //         expect($scope.slices).toEqual(8);
-    //     });
-    //     it('Should instantiate $scope.lastRequestedFlavor', function () {
-    //         expect($scope.lastRequestedFlavor).toBeUndefined();
-    //     });
-    // });
-
-    // describe('Action Handlers', function () {
-    //     describe('eatSlice', function () {
-    //         it('Should decrement the number of slices', function () {
-    //             expect($scope.slices).toEqual(8);
-    //             $scope.eatSilce();
-    //             expect($scope.slices).toEqual(7);
-    //         });
-    //         it('Should do nothing when slices is 0', function () {
-    //             expect($scope.slices).toEqual(8);
-    //             $scope.slices = 0;
-    //             expect($scope.slices).toEqual(0);
-    //             $scope.eatSilce();
-    //             expect($scope.slices).toEqual(0);
-    //         });
-    //     });
-    //     describe('requestedFlavor', function () {
-    //         it('Should set $scope.lastRequestedFlavor to the passed in argument', function () {
-    //             expect($scope.lastRequestedFlavor).toBeUndefined();
-    //             $controller.requestFlavor("Cherry");
-    //             expect($scope.lastRequestedFlavor).toEqual("Cherry");
-    //         });
-    //
-    //     });
-    // });
-
-
 });
